7f236441e4cddc4c188c91311626d920354926a7
raj847 <aryadevaraj19@gmail.com>
2021-03-17 13:11:09 +0700

    aryaaaa

---
+++ tugasfunc.js
@@ -0,0 +1,12 @@
+// let shoutOut = ()=>'Hello Function!'
+
+// console.log(shoutOut());
+
+function calculateMultiply(num1,num2){
+    return num1*num2;
+}
+
+let num1 = 6;
+let num2 = 5;
+let hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
