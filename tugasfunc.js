3ab0e1912398ff18e6b0aeb0a38272acebaf40db
raj847 <aryadevaraj19@gmail.com>
2021-03-17 13:29:04 +0700

    aryaaa

--- tugasfunc.js
+++ tugasfunc.js
@@ -2,11 +2,22 @@
 
 // console.log(shoutOut());
 
-function calculateMultiply(num1,num2){
-    return num1*num2;
+// function calculateMultiply(num1,num2){
+//     return num1*num2;
+// }
+
+// let num1 = 6;
+// let num2 = 5;
+// let hasilPerkalian = calculateMultiply(num1,num2);
+// console.log(hasilPerkalian);
+function processSentence(){
+ return `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}`
 }
 
-let num1 = 6;
-let num2 = 5;
-let hasilPerkalian = calculateMultiply(num1,num2);
-console.log(hasilPerkalian);
+let name = "Agus";
+let age = 30;
+let address = "Jln. Malioboro, Yogjakarta";
+let hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence);
